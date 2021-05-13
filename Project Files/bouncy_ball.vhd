01cff86ad2903aff3514f3dc91d8c70056cb77ef
Shreya Singaraju <ssin733@aucklanduni.ac.nz>
2021-05-13 19:16:37 +1200

    Fixed pipe display and added second pipe
    Need to fix multiple pipes display. Can only display multiple pipes on different colour signals - nothing shows up if two pipes are on the same colour signal.

--- Project Files/bouncy_ball.vhd
+++ Project Files/bouncy_ball.vhd
@@ -22,8 +22,12 @@ SIGNAL ball_y_motion			: std_logic_vector(9 DOWNTO 0);
 
 SIGNAL pipeTop1				: std_logic;
 SIGNAL pipeBot1				: std_logic;
+SIGNAL pipeTop2				: std_logic;
+SIGNAL pipeBot2				: std_logic;
+SIGNAL pipeSpace 				: std_logic_vector(9 DOWNTO 0);
 SIGNAL pipeSize 				: std_logic_vector(9 DOWNTO 0);
-SIGNAL pipeYSize 				: std_logic_vector(9 DOWNTO 0); 
+SIGNAL pipeTopYSize 			: std_logic_vector(9 DOWNTO 0); 
+SIGNAL pipeBotYSize 			: std_logic_vector(9 DOWNTO 0); 
 SIGNAL pipeTopYPos			: std_logic_vector(9 DOWNTO 0);
 SIGNAL pipeBotYPos			: std_logic_vector(9 DOWNTO 0);
 SiGNAL pipe_x_pos				: std_logic_vector(10 DOWNTO 0);
@@ -35,13 +39,14 @@ size <= CONV_STD_LOGIC_VECTOR(8,10);
 -- ball_x_pos and ball_y_pos show the (x,y) for the centre of ball
 ball_x_pos <= CONV_STD_LOGIC_VECTOR(250,11);
 
-pipeSize <= CONV_STD_LOGIC_VECTOR(30,10);
-pipeYSize <= CONV_STD_LOGIC_VECTOR(100,10);
+pipeSpace <= CONV_STD_LOGIC_VECTOR(150,10);
+pipeSize <= CONV_STD_LOGIC_VECTOR(20,10);
+pipeTopYSize <= CONV_STD_LOGIC_VECTOR(170,10);
+pipeBotYSize <= CONV_STD_LOGIC_VECTOR(250,10);
 
 
-pipeTopYPos <= CONV_STD_LOGIC_VECTOR(94,10);
+pipeTopYPos <= CONV_STD_LOGIC_VECTOR(100,10);
 pipeBotYPos <= CONV_STD_LOGIC_VECTOR(400,10);
-
 
 
 
@@ -51,17 +56,36 @@ ball_on <= '1' when ( ('0' & pixel_column + size >= '0' & ball_x_pos)
 					and ('0' & pixel_row <= ball_y_pos + size) )  else	-- y_pos - size <= pixel_row <= y_pos + size
 			  '0';
 			  
-pipeTop1 <= '1' when ( ('0' & pixel_column + pipeSize >= '0' & pipe_x_pos) 
-					and ('0' & pixel_column <= '0' & pipe_x_pos + pipeSize) 	-- x_pos - size <= pixel_column <= x_pos + size
-					and (pixel_row + pipeSize + "0001000000" >= '0' & pipeTopYPos) 
-					and ('0' & pixel_row <= pipeTopYPos + pipeSize) )  else	-- y_pos - size <= pixel_row <= y_pos + size
+--pipeTop1 <= '1' when ( ('0' & pixel_column + pipeSize >= '0' & pipe_x_pos) 
+--					and ('0' & pixel_column <= '0' & pipe_x_pos + pipeSize) 	-- x_pos - size <= pixel_column <= x_pos + size
+--					--and (pixel_row + pipeTopYSize + "0001000000" >= '0' & pipeTopYPos)
+--					and ('0' & pixel_row <= pipeTopYPos + pipeTopYSize) )  else	-- y_pos - size <= pixel_row <= y_pos + size
+--			  '0';	
+--			  
+--pipeBot1 <= '1' when ( ('0' & pixel_column + pipeSize >= '0' & pipe_x_pos) 
+--					and ('0' & pixel_column <= '0' & pipe_x_pos + pipeSize) 	-- x_pos - size <= pixel_column <= x_pos + size
+--					--and (pixel_row + pipeBotYSize + "0001000000" >= '0' & pipeBotYPos) 
+--					and ('0' & pixel_row <= pipeBotYPos + pipeBotYSize) )  else	-- y_pos - size <= pixel_row <= y_pos + size
+--			  '0';	
+		
+pipeBot1 <= '1' when ( pixel_row >= pipeTopYSize and pixel_row <= pipeBotYSize) else	-- y_pos - size <= pixel_row <= y_pos + size
 			  '0';	
 			  
-pipeBot1 <= '1' when ( ('0' & pixel_column + pipeSize >= '0' & pipe_x_pos) 
-					and ('0' & pixel_column <= '0' & pipe_x_pos + pipeSize) 	-- x_pos - size <= pixel_column <= x_pos + size
-					and (pixel_row + pipeSize + "0001000000" >= '0' & pipeBotYPos) 
-					and ('0' & pixel_row <= pipeBotYPos + "0001000000") )  else	-- y_pos - size <= pixel_row <= y_pos + size
-			  '0';		  
+pipeTop1 <= '0' when (( '1' & pixel_column + pipeSize >= '1' & pipe_x_pos) and 
+							('1' & pixel_column <= '1' & pipe_x_pos + pipeSize) and
+							(pixel_row + pipeSize >= '1' & pipeTopYPos) and
+							('1' & pixel_row <= pipeTopYPos + pipeSize)) else	-- y_pos - size <= pixel_row <= y_pos + size
+							'1';	
+							
+pipeBot2 <= '1' when ( pixel_row >= pipeTopYSize + 50 and pixel_row <= pipeBotYSize + 90) else	-- y_pos - size <= pixel_row <= y_pos + size
+			  '0';	
+			  
+pipeTop2 <= '0' when (( '1' & pixel_column + pipeSize >= '1' & pipe_x_pos + pipeSpace) and 
+							('1' & pixel_column <= '1' & pipe_x_pos + pipeSpace + pipeSize) and
+							(pixel_row + pipeSize >= '1' & pipeTopYPos) and
+							('1' & pixel_row <= pipeTopYPos + pipeSize)) else	-- y_pos - size <= pixel_row <= y_pos + size
+							'1';	 
+
 
 
 -- Colours for pixel data on video signal
@@ -77,7 +101,7 @@ begin
 
 	Red <= (not pipeTop1) and (not pipeBot1);
 	Green <= (not ball_on) and (not textOutput);
-	Blue <= '1';
+	Blue <= (not pipeTop2) and (not pipeBot2);
 
 	-- Move ball once every vertical sync
 	if (rising_edge(vert_sync)) then	
