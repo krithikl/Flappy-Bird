de58ce5ae36b8a0a91d3935036d6bcb29a08872d
klek990 <68366597+klek990@users.noreply.github.com>
2021-05-14 14:06:18 +1200

    Shreya's Commit - Added more pipes and background colour

--- Project Files/bouncy_ball.vhd
+++ Project Files/bouncy_ball.vhd
@@ -15,6 +15,7 @@ END bouncy_ball;
 architecture behavior of bouncy_ball is
 
 SIGNAL ball_on					: std_logic;
+SIGNAL background          : std_logic;
 SIGNAL size 					: std_logic_vector(9 DOWNTO 0);  
 SIGNAL ball_y_pos				: std_logic_vector(9 DOWNTO 0);
 SiGNAL ball_x_pos				: std_logic_vector(10 DOWNTO 0);
@@ -24,6 +25,8 @@ SIGNAL pipeTop1				: std_logic;
 SIGNAL pipeBot1				: std_logic;
 SIGNAL pipeTop2				: std_logic;
 SIGNAL pipeBot2				: std_logic;
+SIGNAL pipeTop3				: std_logic;
+SIGNAL pipeBot3				: std_logic;
 SIGNAL pipeSpace 				: std_logic_vector(9 DOWNTO 0);
 SIGNAL pipeSize 				: std_logic_vector(9 DOWNTO 0);
 SIGNAL pipeTopYSize 			: std_logic_vector(9 DOWNTO 0); 
@@ -34,9 +37,9 @@ SiGNAL pipe_x_pos				: std_logic_vector(10 DOWNTO 0);
 SIGNAL pipe_x_motion			: std_logic_vector(10 DOWNTO 0);
 
 
-
 SIGNAL collision : std_logic;
 SIGNAL score : std_logic_vector(5 DOWNTO 0) := "110000";
+signal pipes : std_logic;
 
 
 BEGIN           
@@ -45,7 +48,7 @@ size <= CONV_STD_LOGIC_VECTOR(8,10);
 -- ball_x_pos and ball_y_pos show the (x,y) for the centre of ball
 ball_x_pos <= CONV_STD_LOGIC_VECTOR(250,11);
 
-pipeSpace <= CONV_STD_LOGIC_VECTOR(150,10);
+pipeSpace <= CONV_STD_LOGIC_VECTOR(200,10);
 pipeSize <= CONV_STD_LOGIC_VECTOR(20,10);
 pipeTopYSize <= CONV_STD_LOGIC_VECTOR(170,10);
 pipeBotYSize <= CONV_STD_LOGIC_VECTOR(250,10);
@@ -62,17 +65,9 @@ ball_on <= '1' when ( ('0' & pixel_column + size >= '0' & ball_x_pos)
 					and ('0' & pixel_row <= ball_y_pos + size) )  else	-- y_pos - size <= pixel_row <= y_pos + size
 			  '0';
 			  
---pipeTop1 <= '1' when ( ('0' & pixel_column + pipeSize >= '0' & pipe_x_pos) 
---					and ('0' & pixel_column <= '0' & pipe_x_pos + pipeSize) 	-- x_pos - size <= pixel_column <= x_pos + size
---					--and (pixel_row + pipeTopYSize + "0001000000" >= '0' & pipeTopYPos)
---					and ('0' & pixel_row <= pipeTopYPos + pipeTopYSize) )  else	-- y_pos - size <= pixel_row <= y_pos + size
---			  '0';	
---			  
---pipeBot1 <= '1' when ( ('0' & pixel_column + pipeSize >= '0' & pipe_x_pos) 
---					and ('0' & pixel_column <= '0' & pipe_x_pos + pipeSize) 	-- x_pos - size <= pixel_column <= x_pos + size
---					--and (pixel_row + pipeBotYSize + "0001000000" >= '0' & pipeBotYPos) 
---					and ('0' & pixel_row <= pipeBotYPos + pipeBotYSize) )  else	-- y_pos - size <= pixel_row <= y_pos + size
---			  '0';	
+background <= '1' when (pixel_row >= 0 and pixel_row <= 479) else
+				  '0';
+			  
 		
 pipeBot1 <= '1' when ( pixel_row >= pipeTopYSize and pixel_row <= pipeBotYSize) else	-- y_pos - size <= pixel_row <= y_pos + size
 			  '0';	
@@ -83,31 +78,44 @@ pipeTop1 <= '0' when (( '1' & pixel_column + pipeSize >= '1' & pipe_x_pos) and
 							('1' & pixel_row <= pipeTopYPos + pipeSize)) else	-- y_pos - size <= pixel_row <= y_pos + size
 							'1';	
 							
-pipeBot2 <= '1' when ( pixel_row >= pipeTopYSize + 50 and pixel_row <= pipeBotYSize + 90) else	-- y_pos - size <= pixel_row <= y_pos + size
+pipeBot2 <= '1' when ( pixel_row >= pipeTopYSize + 80 and pixel_row <= pipeBotYSize + 80) else	-- y_pos - size <= pixel_row <= y_pos + size
 			  '0';	
 			  
 pipeTop2 <= '0' when (( '1' & pixel_column + pipeSize >= '1' & pipe_x_pos + pipeSpace) and 
 							('1' & pixel_column <= '1' & pipe_x_pos + pipeSpace + pipeSize) and
 							(pixel_row + pipeSize >= '1' & pipeTopYPos) and
 							('1' & pixel_row <= pipeTopYPos + pipeSize)) else	-- y_pos - size <= pixel_row <= y_pos + size
-							'1';	 
-
-
+							'1';
+					
+pipeBot3 <= '1' when ( pixel_row >= pipeTopYSize - 50 and pixel_row <= pipeBotYSize - 50) else	-- y_pos - size <= pixel_row <= y_pos + size
+			  '0';
+			  
+pipeTop3 <= '0' when (( '1' & pixel_column + pipeSize >= '1' & pipe_x_pos + pipeSpace + pipeSpace) and 
+							('1' & pixel_column <= '1' & pipe_x_pos + pipeSpace + pipeSpace + pipeSize) and
+							(pixel_row + pipeSize >= '1' & pipeTopYPos) and
+							('1' & pixel_row <= pipeTopYPos + pipeSize)) else	-- y_pos - size <= pixel_row <= y_pos + size
+							'1';
+			  
+pipes <= (not pipeBot1 and not pipeTop1) or (not pipeBot2 and not pipeTop2) or (not pipeBot3 and not pipeTop3);
 
 -- Colours for pixel data on video signal
 -- Changing the background and ball colour by pushbuttons
 
 
 Move_Ball: process (vert_sync)
-variable tick : std_logic := '0';
 begin
 --	Red <= (not ball_on) and (not textOutput);
 --	Green <= not sw0;
 --	Blue <= (not pipe_on);
 
-	Red <= (not pipeTop1) and (not pipeBot1);
-	Green <= (not ball_on) and (not textOutput);
-	Blue <= (not pipeTop2) and (not pipeBot2);
+--	Red <= not pipes;
+--	Green <= (not ball_on) and (not textOutput);
+--	Blue <= '0';
+
+	Red <= not background or ball_on or pipes;
+	Green <=  not background or ball_on;
+	Blue <= not background or (textOutput) ;
+
 
 	-- Move ball once every vertical sync
 	if (rising_edge(vert_sync)) then	
@@ -129,7 +137,6 @@ begin
 			ball_y_pos <= ball_y_pos + ball_y_motion - "000000111";
 			
 		elsif (leftButton = '0') then
-			tick := '0';
 			if (ball_y_pos <= size) then
 				ball_y_motion <= - CONV_STD_LOGIC_VECTOR(2,10);
 			elsif ('0' & ball_y_pos >= CONV_STD_LOGIC_VECTOR(479, 10) - size) then
