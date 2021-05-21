e0554728a054c59f675229699d82670bf60bac79
klek990 <68366597+klek990@users.noreply.github.com>
2021-05-21 18:55:35 +1200

    Score working

--- Project Files/bouncy_ball.vhd
+++ Project Files/bouncy_ball.vhd
@@ -11,7 +11,7 @@ ENTITY bouncy_ball IS
 		 
 		 
 		 	-- for score increment NEW 
-			SIGNAL score_ones_out, score_tens_out : OUT std_logic_vector(5 downto 0);
+			SIGNAL score_ones_out, score_tens_out : OUT std_logic_vector(5 downto 0) := "110000";
 		 
 		 
 		 
@@ -41,24 +41,24 @@ SIGNAL pipeTopGap 			: std_logic_vector(9 DOWNTO 0);
 SIGNAL pipeBotGap 			: std_logic_vector(9 DOWNTO 0); 
 SiGNAL pipe_x_pos				: std_logic_vector(10 DOWNTO 0) := "00000000000";
 SIGNAL pipe_x_motion			: std_logic_vector(10 DOWNTO 0);
+signal pipeXLeft 				: std_logic_vector(10 downto 0) := pipe_x_pos - pipeWidth - ball_x_pos - size - size;
+signal pipeXRight 			: std_logic_vector(10 downto 0) := pipe_x_pos + pipeWidth - ball_x_pos;
 
--- NEW 
-SIGNAL ones_score,tens_score : std_logic_vector(5 downto 0) := "000000";
+
+SIGNAL ones_score,tens_score : std_logic_vector(5 downto 0) := "110000";
 
 
 SIGNAL collision : std_logic := '0';
-SIGNAL score : std_logic_vector(5 DOWNTO 0) := "110000";
-
 signal pipes : std_logic;
 
 signal ballPadding : std_logic_vector(9 downto 0);
+signal resetScore : std_logic_vector(5 downto 0) := "110000";
 
 
 
 BEGIN           
 
 size <= CONV_STD_LOGIC_VECTOR(8,10);
--- ball_x_pos and ball_y_pos show the (x,y) for the centre of ball
 ball_x_pos <= CONV_STD_LOGIC_VECTOR(250,11);
 
 pipeSpacing <= CONV_STD_LOGIC_VECTOR(200,10);
@@ -78,8 +78,10 @@ background <= '1' when (pixel_row >= 0 and pixel_row <= 479) or (pixel_column >=
 				  '0';
 			  
 		
-pipeBot1 <= '1' when ( pixel_row >= pipeTopGap and pixel_row <= pipeBotGap) else	-- y_pos - size <= pixel_row <= y_pos + size
-			  '0';	
+pipeBot1 <= '1' when ( pixel_row >= pipeTopGap and pixel_row <= pipeBotGap) 
+				else	-- y_pos - size <= pixel_row <= y_pos + size
+			  '0';
+			  
 			  
 pipeTop1 <= '0' when (( '1' & pixel_column + pipeWidth >= '1' & pipe_x_pos) and 
 							('1' & pixel_column <= '1' & pipe_x_pos + pipeWidth))
@@ -110,60 +112,80 @@ variable tick : std_logic := '0';
 begin
 
 -- Colour signal assignments
-Red <= (background or ball_on) and (not pipes) and (not textOutput);
-Green <= background or ball_on or pipes;
-if (collision = '1') then
-	Green <=  background or ball_on or pipes or textOutput;
-end if;
-Blue <= background and (not ball_on) and (not pipes) and (not textOutput);
+Red <= (background or ball_on) and (not pipes);
+Green <=  (background or ball_on or pipes) and (not textOutput);
+Blue <= background and (not ball_on) and (not pipes);
+
 
 	-- Move ball once every vertical sync
 	if (rising_edge(vert_sync)) then	
 		if (collision = '0') then
+			
 			pipe_x_motion <= CONV_STD_LOGIC_VECTOR(4,11);
 			pipe_x_pos <= pipe_x_pos - pipe_x_motion;
 		end if;
 			
+			
+			
 		-- Pipe 1 collision
-		if ((ball_y_pos + size + ballPadding >= pipeBotGap) OR ((ball_y_pos <= pipeTopGap + size))) then 
-			if ((ball_x_pos + size  <= pipe_x_pos - pipeWidth - ball_x_pos - size - size) 
-			and (ball_x_pos + size>= pipe_x_pos + pipeWidth - ball_x_pos)) then
+			score_ones_out <= ones_score;
+			
+			if ((ball_y_pos + size + ballPadding >= pipeBotGap) OR ((ball_y_pos <= pipeTopGap + size))) then
+				if ((ball_x_pos + size  <= pipeXLeft) 
+				and (ball_x_pos + size >= pipeXRight)) then
+					 collision <='1';
+					 pipe_x_motion <= CONV_STD_LOGIC_VECTOR(0,11);
+					 ball_y_motion <= CONV_STD_LOGIC_VECTOR(0,10);
+				elsif (collision = '0' and (ball_x_pos + size <= pipeXLeft)) then
+					if (ones_score < resetScore + "000001") then
+						ones_score <= ones_score + "000001";
+					else 
+						ones_score <= ones_score;
+					end if;
+						
+				end if;
+			end if;
+			
+
+
+		--Pipe 2 collision
+		if ((ball_y_pos + size + ballPadding >= pipeBotGap + 80) OR ((ball_y_pos - size + ballPadding <= pipeTopGap + 80))) then 
+			if ((ball_x_pos + size <= pipeXLeft + pipeSpacing) 
+			and (ball_x_pos + size >= pipeXRight + pipeSpacing)) then
 				 collision <='1';
 				 pipe_x_motion <= CONV_STD_LOGIC_VECTOR(0,11);
 				 ball_y_motion <= CONV_STD_LOGIC_VECTOR(0,10);
-
-
-		-- NEW (
-			else 
+			elsif (collision = '0' and (ball_x_pos + size <= pipeXLeft + pipeSpacing)) then
+				if (ones_score < resetScore + "000010") then
 					ones_score <= ones_score + "000001";
-				-- when ones score overflows, score tens will be incremented 
-					if(ones_score = "111001") then
-						tens_score <= tens_score + "000001";
-						ones_score <= "000000";
-					end if;
-			-- )NEW
+					else 
+					ones_score <= ones_score;
+				end if;
 			end if;
 		end if;
 		
-		-- Pipe 2 collision
-		if ((ball_y_pos + size + ballPadding >= pipeBotGap + 80) OR ((ball_y_pos - size + ballPadding <= pipeTopGap + 80))) then 
-			if ((ball_x_pos + size <= pipe_x_pos - pipeWidth - ball_x_pos - size - size + pipeSpacing) 
-			and (ball_x_pos + size >= pipe_x_pos - pipeWidth - ball_x_pos + pipeSpacing)) then
-				 collision <='1';
-				 pipe_x_motion <= CONV_STD_LOGIC_VECTOR(0,11);
-				 ball_y_motion <= CONV_STD_LOGIC_VECTOR(0,10);
-			end if;
-		end if;
+		
 		
 		-- Pipe 3 collision
 		if ((ball_y_pos + size + ballPadding >= pipeBotGap - 50) OR ((ball_y_pos - size + ballPadding <= pipeTopGap - 50))) then 
-			if ((ball_x_pos + size <= pipe_x_pos - pipeWidth - ball_x_pos - size - size + pipeSpacing + pipeSpacing) 
-			and (ball_x_pos + size >= pipe_x_pos - pipeWidth - ball_x_pos + pipeSpacing + pipeSpacing)) then
+			if ((ball_x_pos + size <= pipeXLeft + pipeSpacing + pipeSpacing) 
+				and (ball_x_pos + size >= pipeXRight + pipeSpacing + pipeSpacing)) then
 				 collision <='1';
 				 pipe_x_motion <= CONV_STD_LOGIC_VECTOR(0,11);
 				 ball_y_motion <= CONV_STD_LOGIC_VECTOR(0,10);
+			elsif (collision = '0' and (ball_x_pos + size <= pipeXLeft + pipeSpacing + pipeSpacing)) then
+				if (ones_score < resetScore + "000011") then
+					ones_score <= ones_score + "000001";
+					else 
+					ones_score <= ones_score;
+				end if;
+				
 			end if;
 		end if;
+		
+--		if (pipe_x_pos <= CONV_STD_LOGIC_VECTOR(0,11) + pipeSpacing + pipeSpacing) then
+--			pipe_x_pos <= CONV_STD_LOGIC_VECTOR(639,11);
+--		end if;
 
 			
 			if (leftButton = '1' and collision = '0') then
@@ -175,13 +197,11 @@ Blue <= background and (not ball_on) and (not pipes) and (not textOutput);
 				
 				
 				ball_y_pos <= ball_y_pos + ball_y_motion - "000011111";
+				
+				
 				mouseReset <= '1';
 				
 				
---			elsif (leftButton = '1' and collision = '0' and tick = '1') then
---				ball_y_motion <= -CONV_STD_LOGIC_VECTOR(2,10);	
---				ball_y_pos <= ball_y_pos - ball_y_motion;
---				
 				
 			elsif (leftButton = '0' and collision = '0') then
 				if (ball_y_pos <= size) then
@@ -206,18 +226,11 @@ Blue <= background and (not ball_on) and (not pipes) and (not textOutput);
 			end if ;
 			
 			
-				
-				
---
----- score calculation 
---			if (collision ='0') then 
---				score <= score+1;
---				elsif (collision ='1') then 
---				 score <= score;
---			end if; 	
 			
 			
-		
+			
+				
+
 	end if;
 end if;
 end process Move_Ball;
